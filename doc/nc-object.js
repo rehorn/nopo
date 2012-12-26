99c690d0f53fbd3f9267a639a4f9cffd10008858
rehorn <sysucgh@163.com>
2012-12-26 20:39:27 +0800

    test

---
+++ doc/nc-object.js
@@ -0,0 +1,34 @@
+{
+    'NOPO_FILE': 'abc.json',
+    'tasks': [
+        {
+            'taskName': 'name',
+            'cmds': [
+                {
+                    'cmd': 'cmd_abc',
+                    'source': './',
+                    'target': './cdn/'
+                    'option': {
+                        'param': true
+                    }
+                },
+                {
+                    'cmd': 'cmd_abc',
+                    'source': './',
+                    'target': './cdn/'
+                    'option': {
+                        'param': true
+                    }
+                },
+                {
+                    'cmd': 'cmd_abc',
+                    'source': './',
+                    'target': './cdn/'
+                    'option': {
+                        'param': true
+                    }
+                }
+            ]
+        }
+    ]
+}
