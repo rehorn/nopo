4cebd66f14f74887a6a0919a0eebbda5dfbeb578
rehorn <sysucgh@163.com>
2013-02-22 17:25:26 +0800

    test case for deploy

---
+++ test/nopo-test2/nopo.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "nopoParser": "nopo-node",
+    "tasks": {
+        "deploy":{
+            "cmd": "deploy",
+            "source": "**",
+            "target": "/htdocs/",
+            "@deploy": {
+                "type": "ftp",
+                "host": "223.7.143.233​",
+                "port": "21",
+                "user": "hmu046233",
+                "password": "member99"
+            }
+        }
+    }
+}
