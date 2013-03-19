531af4e2e74b969ba13d13c391f215147b8247e2
rehorn <sysucgh@163.com>
2013-03-20 01:39:21 +0800

    tango.js

--- test/nopo-test3/nopo.js
+++ test/nopo-test3/nopo.js
@@ -4,7 +4,6 @@ module.exports = {
         "sprite":{
             "cmd": "sprite",
             "source": "css/",
-            "target": "css/",
             "@sprite": {
                 'imgRoot': "img/"
             }
