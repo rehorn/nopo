ceda8d33f173e4445ee7b84e24e1e08becebfc81
rehorn <sysucgh@163.com>
2013-03-28 01:37:28 +0800

    css enhance & test

---
+++ test/nopo-test6/nopo.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "tasks": {
+        "enhancecss":{
+            "cmd": "css",
+            "source": "css/",
+            "@css": {
+                "stamp": 1,
+                "cryptedStamp": 1,
+                "assetHosts": "http://qplus[0,1,2,3,4,5,6,7,8,9].idqqimg.com/pub/",
+                "dataUri": 1,
+                "exclude": ["img/test3.png"],
+                "skipDuplicate": 1
+            }
+        }
+    }
+}
