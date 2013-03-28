1e3e5ad4b2bcc5eea32baada229557bad5ce511a
rehorn <sysucgh@163.com>
2013-03-29 03:15:17 +0800

    update css & htmlrefs cmd

--- test/nopo-test6/nopo.js
+++ test/nopo-test6/nopo.js
@@ -7,6 +7,7 @@ module.exports = {
                 "stamp": 1,
                 "cryptedStamp": 1,
                 "assetHosts": "http://qplus[0,1,2,3,4,5,6,7,8,9].idqqimg.com/pub/",
+                "rev": 1,
                 "dataUri": 1,
                 "exclude": ["img/test3.png"],
                 "skipDuplicate": 1
